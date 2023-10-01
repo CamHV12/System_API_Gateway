cd7bfa57cb84b3fe4efd65e31ec33b1e346ad355
CamHV12 <huavancam2001@gmail.com>
2023-10-01 19:37:25 +0700

    first commit

---
+++ System-microservice-gateway/src/main/java/com/example/ExampleController.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/first")
+public class ExampleController {
+	@GetMapping("/message")
+    public String test() {
+        return "Hello this is my Second Service";
+    }
+}
